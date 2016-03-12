7222adacff67e95028c7a687a317557fbf091461
felixdae <felixdae@gmail.com>
2016-03-12 20:14:27 +0800

    rm vim swp file

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -58,8 +58,6 @@ J = 1/2*sum(sum((X * Theta' - Y) .^ 2 .* R)) + lambda/2*(sum(sum(Theta .^ 2)) + 
 X_grad += ((X * Theta' - Y) .* R) * Theta + lambda*X; % .* R
 Theta_grad = ((X * Theta' - Y) .* R)' * X + lambda*Theta; % .* R
 
-
-
 % =============================================================
 
 grad = [X_grad(:); Theta_grad(:)];
